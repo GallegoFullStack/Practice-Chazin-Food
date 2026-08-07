b5961c94e96cecb03d9540852ce49d8f0922d655
AGPSYSTEM <gomezpavas34@gmail.com>
2026-08-07 18:11:56 -0500

    feat: implementar módulo de fichas técnicas (CRUD completo)

--- backend/src/presentation/routes/fichaTecnicaRoutes.js
+++ backend/src/presentation/routes/fichaTecnicaRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { getFichas, getFichaById, createFicha, updateFicha, deleteFicha } = require('../controllers/fichaTecnicaController');
 
 router.route('/')
-  .get(getFichas)
-  .post(createFicha);
+    .get(getFichas)
+    .post(createFicha);
 
 router.route('/:id')
-  .get(getFichaById)
-  .put(updateFicha)
-  .delete(deleteFicha);
+    .get(getFichaById)
+    .put(updateFicha)
+    .delete(deleteFicha);
 
 module.exports = router;
